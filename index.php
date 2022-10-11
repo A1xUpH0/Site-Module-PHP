e7d2740b030fb437943f791586637cb00bf9dd21
David Allais <davidallais2001@gmail.com>
2022-10-11 11:30:19 +0200

    Add basics files

---
+++ index.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <title>Module PHP David</title>
+    <link rel="stylesheet" href="./CSS/style.css">
+</head>
+<body>
+    <?php
+    echo "Bonjour, je suis un script PHP !";
+    ?>
+</body>
+</html>
